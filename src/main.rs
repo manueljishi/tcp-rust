97d67daa06e57c4f8a702f53d0e78d399754c06b
Manel Eljishi <meljishi@power-electronics.com>
2023-09-19 18:37:02 +0200

    First commit, create fictional nic in userspace

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main()-> io::Result<()>{
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1054];
+    loop{
+        let nbytes = nic.recv(&mut buf[..])?;
+        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    }
+    Ok(())
+}
